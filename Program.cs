193498962a01b0e3cb2dc8c8bcacf6e4d79d67a3
AlicaWalker2004 <daniella@email.com>
2024-05-04 10:32:23 +0400

    add +

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿int a;
+int b;
+int c = new int();
+int znak;
+
+Console.Write("Введите A: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите B: ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("1 - сложение;");
+Console.Write("Введите нужную цифру: ");
+znak = Convert.ToInt32(Console.ReadLine());
+
+if (znak == 1)
+{
+    c = a + b;
+}
+
+
+Console.WriteLine("Ответ: " + c);
